25f94c1b04fc0374bac68ca0c3e5814b7dd174af
phybrain <692114871@qq.com>
2017-07-12 13:43:53 +0800

    First1.0.0

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  First
+//
+//  Created by 陈小鹏 on 2017/7/11.
+//  Copyright © 2017年 陈小鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
